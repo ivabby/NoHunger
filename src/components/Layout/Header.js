62c706182a4536cb8177e189e23ce64f4edb12b7
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2022-09-11 20:47:18 +0530

    Updated App Name

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>NoHunger</h1>
         <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
